3f4a26260aa67dbad3419596cbd413e872294262
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-04-02 13:15:05 +0530

    Completed the RealTime message webApp - WebScoket

--- index.js
+++ index.js
@@ -8,24 +8,25 @@ const port = 8000;
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-    console.log("New user connected", socket.id);
-    
-    socket.on("message", (msg) => {
-        io.emit("message", msg); 
-    });
+  console.log("New user connected", socket.id);
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
-    });
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+    console.log("Message received:", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
 });
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");  // Send the index.html file
+  res.sendFile(__dirname + "/public/index.html"); // Send the index.html file
 });
 
 // Start the server
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
