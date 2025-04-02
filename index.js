0a7a3c2fc4967acb237db5af1fc5bc1e22681c9f
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-04-02 12:53:55 +0530

    Added Input field in the frontend

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const port = 8000;
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("New user connected", socket.id);
+    
+    socket.on("message", (msg) => {
+        io.emit("message", msg); 
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
 
 app.use(express.static("public"));
 
-server.listen({ port }, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");  // Send the index.html file
 });
 
-
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
